7c334a726f9181c4454bdf5ad5d9428f58e561b0
Miles Morlock <milesmorlockcode@gmail.com>
2022-04-02 18:48:44 -0400

    Styled the top-most nav bar

--- src/components/TopNavBar.js
+++ src/components/TopNavBar.js
@@ -1,40 +1,19 @@
 import React from 'react';
-import { Container, Row, Col } from 'reactstrap';
 
 const TopNavBar = () => {
     return (
-        <Container fluid className='topNavBar'>
-            <Row>
-                <Col>
-                    <Row>
-                        <Col>
-                            <a href="../index.js"><p>HOME</p></a>
-                        </Col>
-                        <Col>
-                            <a href="../index.js"><p>PLANS</p></a>
-                        </Col>
-                        <Col>
-                            <a href="../index.js"><p>PRICING</p></a>
-                        </Col>
-                        <Col>
-                            <a href="../index.js"><p>BLOG</p></a>
-                        </Col>
-                        <Col>
-                            <a href="../index.js"><p>DOCUMENTATION</p></a>
-                        </Col>
-                        <Col>
-                            <a href="../index.js"><p>COMPANY</p></a>
-                        </Col>
-                    </Row>
-                </Col>
-                <Col style={{display: 'flex', alignItems: 'center'}}>
-                    <div style={{height: 10.0, width: 10.0, borderRadius: '100%', backgroundColor: '#1ec072'}} />
-                </Col>
-            </Row>
-
-        </Container>
-
-    )
+        <div className="topNavBar">
+            <div className='topNavBarLeft'>
+                <a href="../index.js"><p>HOME</p></a>
+                <a href="../index.js"><p>PLANS</p></a>
+                <a href="../index.js"><p>PRICING</p></a>
+                <a href="../index.js"><p>BLOG</p></a>
+                <a href="../index.js"><p>DOCUMENTATION</p></a>
+                <a href="../index.js"><p>COMPANY</p></a>
+            </div>
+            <div className='topNavBarRight' />
+        </div>
+    );
 }
 
 export default TopNavBar;
