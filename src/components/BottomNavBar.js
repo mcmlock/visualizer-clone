2785492973fce053ea0f940c87f866e66db01d5a
Miles Morlock <milesmorlockcode@gmail.com>
2022-04-02 19:15:24 -0400

    Styled the bottom navbar

--- src/components/BottomNavBar.js
+++ src/components/BottomNavBar.js
@@ -1,37 +1,24 @@
 import React from 'react';
-import { Container, Row, Col } from 'reactstrap';
 
 const BottomNavBar = () => {
     return (
-        <Container fluid>
-            <Row>
-                <Col>
-                    <a href="../index.js"><p>TODAY</p></a>
-                </Col>
-                <Col>
-                    <a href="../index.js"><p>TAGS</p></a>
-                </Col>
-                <Col>
-                    <a href="../index.js"><p>STATS</p></a>
-                </Col>
-                <Col>
-                    <a href="../index.js"><p>ANALYSIS</p></a>
-                </Col>
-                <Col>
-                    <a href="../index.js"><p>ALERTS</p></a>
-                </Col>
-                <Col>
-                    <Row>
-                        <Col>
-                            <a href="../index.js"><p>LOG IN</p></a>
-                        </Col>
-                        <Col>
-                            <a href="../index.js"><p>&gt; SIGN IN</p></a>
-                        </Col>
-                    </Row>
-                </Col>
-            </Row>
-        </Container>
+        <div className='bottomNavBar'>
+            <div className='bottomNavBarLeft'>
+                <a href="../index.js"><p>TODAY</p></a>
+                <a href="../index.js"><p>TAGS</p></a>
+                <a href="../index.js"><p>STATS</p></a>
+                <a href="../index.js"><p>ANALYSIS</p></a>
+                <a href="../index.js"><p>ALERTS</p></a>
+            </div>
+            <div className='bottomNavBarRight'>
+                <a href="../index.js"><p>LOG IN</p></a>
+                <div className="signUpButton">
+                    <a href="../index.js"><p>&gt; SIGN UP</p></a>
+                </div>
+            </div>
+        </div>
+
+
     );
 }
 
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,56 +6,17 @@ import { faLinkedinIn, faTwitter } from '@fortawesome/free-brands-svg-icons';
 const Footer = () => {
     return (
         <div className='footer'>
-            <Container>
-                <Row>
-                    <Col>
-                        <h3>VISUALIZER</h3>
-                        <ul>
-                            <li>Today</li>
-                            <li>Tags</li>
-                            <li>Stats</li>
-                            <li>Pulse</li>
-                        </ul>
-                    </Col>
-                    <Col>
-                        <h3>RESOURCES</h3>
-                        <ul>
-                            <li>Documentation</li>
-                            <li>Integrations</li>
-                            <li>Service Status</li>
-                        </ul>
-                    </Col>
-                    <Col>
-                        <h3>
-                            COMPANY
-                        </h3>
-                        <ul>
-                            <li>Careers</li>
-                        </ul>
-                    </Col>
-                    <Col>
-                        <h3>BLOG</h3>
-                    </Col>
-                    <Col>
-                        <h3>PRICING</h3>
-                    </Col>
-                </Row>
-                <Row>
-                    <Col>
-                        <div>
-                            <FontAwesomeIcon icon={faTwitter} color="white" />
-                            <FontAwesomeIcon icon={faLinkedinIn} color="white" />
-                        </div>
-                        <p>Copyright © 2021 GreyNoise</p>
-                        <ul>
-                            <li>Terms</li>
-                            <li>Privacy</li>
-                            <li>Cookie</li>
-                            <li>Patents</li>
-                        </ul>
-                    </Col>
-                </Row>
-            </Container>
+            <div>
+                <FontAwesomeIcon icon={faTwitter} color="white" />
+                <FontAwesomeIcon icon={faLinkedinIn} color="white" />
+            </div>
+            <p>Copyright © 2021 GreyNoise</p>
+            <ul>
+                <li>Terms</li>
+                <li>Privacy</li>
+                <li>Cookie</li>
+                <li>Patents</li>
+            </ul>
         </div>
     );
 }
