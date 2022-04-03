ec9adc456e720bf9d61fcfceeaffb78281aa69bc
Miles Morlock <milesmorlockcode@gmail.com>
2022-04-03 11:05:07 -0400

    Adjust the layout for medium screen sizes

--- src/components/BottomNavBar.js
+++ src/components/BottomNavBar.js
@@ -1,22 +1,30 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
 
 const BottomNavBar = () => {
     return (
-        <div className='bottomNavBar'>
-            <div className='bottomNavBarLeft'>
-                <a href="../index.js"><p>TODAY</p></a>
-                <a href="../index.js"><p>TAGS</p></a>
-                <a href="../index.js"><p>STATS</p></a>
-                <a href="../index.js"><p>ANALYSIS</p></a>
-                <a href="../index.js"><p>ALERTS</p></a>
+        <div>
+            <div className="bottomNavBarMobile">
+                <FontAwesomeIcon icon={faBars} className="expand-btn" />
             </div>
-            <div className='bottomNavBarRight'>
-                <a href="../index.js"><p>LOG IN</p></a>
-                <div className="signUpButton">
-                    <a href="../index.js"><p>&gt; SIGN UP</p></a>
+            <div className='bottomNavBarDesktop'>
+                <div className='bottomNavBarLeft'>
+                    <a href="../index.js"><p>TODAY</p></a>
+                    <a href="../index.js"><p>TAGS</p></a>
+                    <a href="../index.js"><p>STATS</p></a>
+                    <a href="../index.js"><p>ANALYSIS</p></a>
+                    <a href="../index.js"><p>ALERTS</p></a>
+                </div>
+                <div className='bottomNavBarRight'>
+                    <a href="../index.js"><p>LOG IN</p></a>
+                    <div className="signUpButton">
+                        <a href="../index.js"><p>&gt; SIGN UP</p></a>
+                    </div>
                 </div>
             </div>
         </div>
+
 
 
     );
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -12,7 +12,6 @@ const Footer = () => {
                     <a href="../index.js"><p>Tags</p></a>
                     <a href="../index.js"><p>Stats</p></a>
                     <a href="../index.js"><p>Pulse</p></a>
-
                 </div>
                 <div className='footer-menu-col'>
                     <h6>RESOURCES</h6>
@@ -31,17 +30,21 @@ const Footer = () => {
                     <a href="../index.js"><h6>Pricing</h6></a>
                 </div>
             </div>
+            <hr className='footer-hr' style={{color: 'white', margin: '20.0px auto 20.0px auto', height: '0.5px', width: 650.0}}/>
             <div className='info-div'>
                 <div className='icon-row'>
                 <a href="../index.js"><FontAwesomeIcon icon={faTwitter} className='social-icon' /></a>
                 <a href="../index.js"><FontAwesomeIcon icon={faLinkedinIn} className='social-icon' /></a>
                 </div>
-                <p style={{fontSize: 9.0, margin: 'auto'}}>Copyright © 2021 GreyNoise</p>
+                <p className='copyright-p'>Copyright © 2021 GreyNoise</p>
                 <div className='bottommostRow'>
-                    <a href="../index.js"><p className='border-right-active'>Terms</p></a>
-                    <a href="../index.js"><p className='border-right-active'>Privacy</p></a>
-                    <a href="../index.js"><p className='border-right-active'>Cookie</p></a>
-                    <a href="../index.js"><p>Patent</p></a>
+                    <a href="../index.js"><p>Terms<span className="extra-words"> of Service</span></p></a>
+                    <div className="vertical-line" />
+                    <a href="../index.js"><p>Privacy<span className="extra-words"> Policy</span></p></a>
+                    <div className="vertical-line" />
+                    <a href="../index.js"><p>Cookie</p></a>
+                    <div className="vertical-line" />
+                    <a href="../index.js"><p>Patents</p></a>
                 </div>
             </div>
         </div >
